6296f2826e4874a8c0db444c1605e4c3566c9ade
Vivian969 <vivianwang969@gmail.com>
2025-04-10 15:49:05 -0400

    feat: enhance game UI and reorganize panels

--- public/UI template/app/head.js
+++
@@ -1,9 +0,0 @@
-const Head = () => (
-  <>
-    <title>MetaDroid</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/metadroid-fav.png" />
-  </>
-);
-
-export default Head;
